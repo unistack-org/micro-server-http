74d8cb8538fd8b5904202c6f00aafbd23335f2a5
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-02-02 16:13:39 +0300

    add helper options
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

---
+++ options.go
@@ -0,0 +1,24 @@
+package http
+
+import "context"
+
+type rspCodeKey struct{}
+type rspCodeVal struct {
+	code int
+}
+
+// SetRspCode saves response code in context, must be used by handler to specify http code
+func SetRspCode(ctx context.Context, code int) {
+	if rsp, ok := ctx.Value(rspCodeKey{}).(*rspCodeVal); ok {
+		rsp.code = code
+	}
+}
+
+// GetRspCode used internally by generated http server handler
+func GetRspCode(ctx context.Context) int {
+	var code int
+	if rsp, ok := ctx.Value(rspCodeKey{}).(*rspCodeVal); ok {
+		code = rsp.code
+	}
+	return code
+}
