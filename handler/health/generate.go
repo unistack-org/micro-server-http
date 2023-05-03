ce0abd2de35cb35c57cdbe2365fe89033a81e5ae
Vasiliy Tolstov <v.tolstov@unistack.org>
2023-05-04 02:20:25 +0300

    issue-155: add swagger-ui handler
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- handler/health/generate.go
+++
@@ -1,8 +0,0 @@
-package health
-
-//go:generate sh -c "protoc -I./ -I$(go list -f '{{ .Dir }}' -m go.unistack.org/micro-proto/v3) --go-micro_out='components=micro|http|server',standalone=false,debug=true,paths=source_relative:./ health.proto"
-
-import (
-	// import required packages
-	_ "go.unistack.org/micro-proto/v3/openapiv3"
-)
